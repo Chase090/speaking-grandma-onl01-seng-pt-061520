454d342243459d3d0241a9ebfd45aabc689980c9
Carl Michael Oyam <carloyam21@icloud.com>
2020-06-18 21:54:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,16 @@
+def speak_to_grandma(phrase = "") 
+  puts speak_to_grandma
+      if phrase == "I LOVE YOU GRANDMA!"
+          puts "I LOVE YOU TOO PUMPKIN!"
+      elsif phrase == phrase.upcase
+          puts "NO, NOT SINCE 1938!"
+      else
+  	      puts "HUH?! SPEAK UP, SONNY!"
+      end	  
+end
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
